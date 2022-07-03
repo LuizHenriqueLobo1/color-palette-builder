38b4fc0b1636a22293f19f63dbb05a329fc1c2ed
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-07-03 16:48:37 -0300

    Adicionando página não encontrada

---
+++ src/pages/Not-Found/NotFound.jsx
@@ -0,0 +1,16 @@
+import './NotFound.css';
+
+export default function NotFound() {
+  return (
+    <div className='not-found-container'>
+      <div className='not-found-content'>
+        <h1>
+          Error 404!
+        </h1>
+        <h2>
+          Page not found.
+        </h2>
+      </div>
+    </div>
+  );
+};
--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Build from '../pages/Build/Build.jsx'; 
 import MyPalettes from '../pages/My-Palettes/MyPalettes.jsx';
 import Help from '../pages/Help/Help.jsx';
+import NotFound from '../pages/Not-Found/NotFound.jsx';
 
 export default function MyRoutes() {
   return (
@@ -17,6 +18,10 @@ export default function MyRoutes() {
       <Route 
         path='/help' 
         element={ <Help /> } 
+      />
+      <Route 
+        path='/*'
+        element= { <NotFound /> }
       />
     </Routes>
   );
